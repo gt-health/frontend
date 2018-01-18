c0898c70bf0a21ec71028ea7e114bf95fed94bc7
Trey Schneider <treyschneider@LPATLICLTSCHN7.attlocal.net>
2018-01-17 19:42:57 -0500

    refactor to build form from schema

--- src/components/Case.jsx
+++ src/components/Case.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CaseField from '../containers/case_field_container.js';
+import CaseSection from '../containers/case_section_container.js';
+import { Route, Switch, Link } from 'react-router-dom';
+import Form from "react-jsonschema-form";
+
+import schema from '../helpers/schema.json';
+
+// console.log('myData: ',myData);
+
+// const schema = {
+//   title: "Todo",
+//   type: "object",
+//   required: ["title"],
+//   properties: {
+//     title: {type: "string", title: "Title", default: "A new task"},
+//     done: {type: "boolean", title: "Done?", default: false}
+//   }
+// };
+
+const log = (type) => console.log.bind(console, type);
+
 
 export default class Case extends React.Component {
 
@@ -21,54 +40,37 @@ export default class Case extends React.Component {
 
   render() {
     console.log('*** rendering case component ***');
-    console.log('case: ',this.props.case);
     if (!this.props.case.raw) {
       return (
         <div>Loading</div>
       )
     } else {
       return (
-        <div>
-          <section className="section important">
-          </section>
-          <section className="section patient">
-            <div className="container">
-              <h1 className="title is-1">Patient</h1>
-              <div className="columns">
-                <div className="column">
-                  <CaseField
-                    label="Patient ID Number"
-                    identifier="Patient__ID"
-                    type="string"
-                    editable="true"/>
-                </div>
-                <div className="column">
-                  <CaseField
-                    label="Birth Date"
-                    identifier="Patient__Birth_Date"
-                    type="string"
-                    editable="true"/>
-                </div>
-                <div className="column">
-                  <CaseField
-                    label="Sex"
-                    identifier="Patient__Sex"
-                    type="string"
-                    editable="true"/>
-                </div>
-              </div>
-            </div>
-          </section>
-          <section className="section facility">
-            <div className="container">
-              <h1 className="title is-1">Facility</h1>
-            </div>
-          </section>
-          <section className="section provider">
-            <div className="container">
-              <h1 className="title is-1">Provider</h1>
-            </div>
-          </section>
+        <div className="case">
+          <div className="sidebar">
+            <ul style={{ listStyleType: 'none', padding: 0 }}>
+              <li><Link to={`${this.props.match.url}/provider`}>Provider</Link></li>
+              <li><Link to={`${this.props.match.url}/facility`}>Facility</Link></li>
+              <li><Link to={`${this.props.match.url}/patient`}>Patient</Link></li>
+              <li><button>Save</button></li>
+            </ul>
+          </div>
+          <div className="main">
+            <Switch>
+              <Route
+                path={`${this.props.match.url}/provider`}
+                render={()=><CaseSection section="Provider"/>}
+              />
+              <Route
+                path={`${this.props.match.url}/facility`}
+                render={ () => <CaseSection section="Facility"/> }
+              />
+              <Route
+                path={`${this.props.match.url}/patient`}
+                render={()=><CaseSection section="Patient"/>}
+              />
+            </Switch>
+          </div>
         </div>
       );
     }
